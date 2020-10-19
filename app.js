79d1e21479f35be6fec8270bc83b97373de786c4
Afeef Rabbani <afeef.rabbani@gmail.com>
2020-10-19 21:55:02 +0530

    aa

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const {MONGO_URI} = require("./config/dev");
+// const {MONGO_URI} = require("./config/dev");
 
 //NODEMAILER
 // const nodemailer = require("nodemailer");
@@ -43,13 +43,13 @@ app.use(userRoutes);
 
 
 
-if(process.env.NODE_ENV == "production"){
+// if(process.env.NODE_ENV == "production"){
     app.use(express.static('client/build'))
     // const path = require('path')
     // app.get("*",(req,res)=>{
     //     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     // })
-}
+// }
 
 
 
